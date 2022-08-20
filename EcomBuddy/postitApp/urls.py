b6e30bbec424cc71e8b4fca9fd179a8e0261ca96
Greg Rell <rell.greg@gmail.com>
2022-08-20 00:30:51 -0400

    basic routing

--- EcomBuddy/postitApp/urls.py
+++ EcomBuddy/postitApp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('new_post/', views.new_post, name='create_post')
 ]
 
--- EcomBuddy/postitApp/views.py
+++ EcomBuddy/postitApp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserPost
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello')
 
+def index(request):
+    user_posts = UserPost.objects.all().order_by('-publish_date')
+    context = {'user_posts': user_posts}
+    return render(request, 'postitApp/index.html', context)
+
+def new_post(request):
+    context = {}
+    return render(request, 'postitApp/new_post.html', context)

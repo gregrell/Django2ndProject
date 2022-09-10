269da84329162acb6683daa22b60b638a71ae392
Greg Rell <rell.greg@gmail.com>
2022-09-10 19:16:20 -0400

    Avatar.svg added to static

--- EcomBuddy/postitApp/models.py
+++ EcomBuddy/postitApp/models.py
@@ -8,7 +8,7 @@ class CustomUser(AbstractUser):
     #name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
-    # avatar = models.ImageField(null=True, default="avatar.svg")
+    avatar = models.ImageField(null=True, default="avatar.svg")
 
 
     def __str__(self):
